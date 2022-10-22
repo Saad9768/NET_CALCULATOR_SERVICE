b234b8bb2f5992136a9488a830966215a904a4a6
Saad <skhatimiti@gmail.com>
2022-10-22 04:51:28 +0200

    logg added

--- src/main/java/com/gsg/calculator/service/impl/NetCalculatorServiceImpl.java
+++ src/main/java/com/gsg/calculator/service/impl/NetCalculatorServiceImpl.java
@@ -3,17 +3,22 @@ package com.gsg.calculator.service.impl;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.gsg.calculator.data.TaxRateProvider;
 import com.gsg.calculator.service.NetCalculatorService;
+import com.gsg.calculator.service.NetCalculatorServiceImplTest;
 
 @Service
 public class NetCalculatorServiceImpl implements NetCalculatorService {
+	Logger logger = LoggerFactory.getLogger(NetCalculatorServiceImpl.class);
 
 	// calculate net price from the gross price according to country
 	@Override
 	public Double calculateNetPrice(Double grossPrice, TaxRateProvider countryIso) {
+		logger.debug("Inside calculateNetPrice");
 		if (grossPrice != null && grossPrice.equals(0.0)) {
 			// This logic will return zero. Consumer will handled zero accordingly
 			return 0.0;
